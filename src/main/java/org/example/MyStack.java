1def05f6cc75b6fedb6ebc7ff49f0e4b1e85cdcd
snehamaddhichettyant <snea913@gmail.com>
2024-03-06 19:34:21 -0500

    delegate- Field to hold ArrayList

--- src/main/java/org/example/MyStack.java
+++ src/main/java/org/example/MyStack.java
@@ -4,16 +4,20 @@ import java.util.ArrayList;
 import java.util.EmptyStackException;
 
 public class MyStack<E> extends ArrayList<E> {
+    private ArrayList<E> delegate;
 
+    public MyStack(){
+        delegate = this;
+    }
     public void push(E e) {
-        add(e);
+        delegate.add(e);
         System.out.println(e);
     }
 
     public E pop() {
         if (isEmpty()) throw new EmptyStackException();
         E e = get(size() - 1);
-        remove(size() -1 );
+        delegate.remove(size() -1 );
         return e;
     }
 
